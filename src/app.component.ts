69ecd21096d968fb0cf1de60058d593762ebea7b
mariusz <mariuszjurczenko@gmail.com>
2018-12-18 12:20:15 +0100

    Angular2 odc20

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'converter',
-  template: ` <input type="number" [value]="baseAmount" 
-                (input)="update($event.target.value)"> USD = 
+  template: `Przelicz: <input type="number" [(ngModel)]="baseAmount"> USD 
+             <p>
+              <strong>{{baseAmount}}</strong> USD = 
               <strong>{{targetAmount}}</strong> GBP  
+              </p>
               `,
   styles:[` input[type=number] {
               width: 10ex;
@@ -16,9 +18,8 @@ export class AppComponent {
 
     exchangeRate = 0.70;
     baseAmount = 1;
-    targetAmount = this.exchangeRate;
 
-    update(baseAmount){
-        this.targetAmount = parseFloat(baseAmount) * this.exchangeRate
+    get targetAmount(){
+       return this.baseAmount * this.exchangeRate
     }
 }
